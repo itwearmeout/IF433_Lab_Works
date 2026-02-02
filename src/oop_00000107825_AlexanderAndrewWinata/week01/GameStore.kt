6ca2f16917ee2ff52397ee70bbe580f3981ed829
itwearmeout <crysero2@gmail.com>
2026-02-02 16:20:46 +0700

    week01: (tugas 2) implement discount logic

--- src/oop_00000107825_AlexanderAndrewWinata/week01/GameStore.kt
+++ src/oop_00000107825_AlexanderAndrewWinata/week01/GameStore.kt
@@ -1,6 +1,13 @@
 package oop_00000107825_AlexanderAndrewWinata.week01
 
 fun main(){
-    val gameTitle:String = "Roblox"
-    val price:Int = 19000
+
+    val gameTitle:String = "KCD2"
+
+    val price:Int = 499000
+
+    var discountedPrice = calculateDiscount(price)
+
 }
+
+fun calculateDiscount(price :Int) = if (price > 500000) price-(price*0.20) else price-(price*0.15)
