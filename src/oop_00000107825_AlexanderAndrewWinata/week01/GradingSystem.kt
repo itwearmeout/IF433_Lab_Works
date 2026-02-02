7f077397598e2ecaca86962df3cfcc90c5a18bd2
itwearmeout <crysero2@gmail.com>
2026-02-02 15:43:30 +0700

    week01: add calculateStatus function

--- src/oop_00000107825_AlexanderAndrewWinata/week01/GradingSystem.kt
+++ src/oop_00000107825_AlexanderAndrewWinata/week01/GradingSystem.kt
@@ -14,4 +14,8 @@ fun main(){
     }
 
     println("Grade kamu $grade")
+
+    println("Status: ${calculateStatus(score)}")
 }
+
+fun calculateStatus(score:Int) = if(score>75) "lulus" else "Tidak lulus"
