db77f6e38d47850f13896cc2974530dff97107f0
itwearmeout <crysero2@gmail.com>
2026-02-02 16:34:38 +0700

    week01: (tugas 2) implement null safety for user note

--- src/oop_00000107825_AlexanderAndrewWinata/week01/GameStore.kt
+++ src/oop_00000107825_AlexanderAndrewWinata/week01/GameStore.kt
@@ -8,9 +8,11 @@ fun main(){
 
     var discountedPrice:Int = calculateDiscount(price).toInt()
 
-    printReceipt(title = gameTitle, finalPrice = discountedPrice)
+    var userNote :String? = null
+
+    printReceipt(title = gameTitle, finalPrice = discountedPrice, note = userNote)
 }
 
 fun calculateDiscount(price: Int)= if (price > 500000) price - (price * 0.2) else price - (price * 0.15)
 
-fun printReceipt(title:String, finalPrice:Int) = println("Membeli game $title dengan harga $finalPrice")
+fun printReceipt(title:String, finalPrice:Int, note:String?) = println("Membeli game $title dengan harga $finalPrice, dengan note $note")
