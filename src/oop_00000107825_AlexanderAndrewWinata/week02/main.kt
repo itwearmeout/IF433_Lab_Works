54a59c27fd529778534b893bd5c17b510df070bf
itwearmeout <crysero2@gmail.com>
2026-02-09 15:10:54 +0700

    week02: implement selection logic for constructors

--- src/oop_00000107825_AlexanderAndrewWinata/week02/main.kt
+++ src/oop_00000107825_AlexanderAndrewWinata/week02/main.kt
@@ -5,8 +5,8 @@ import java.util.Scanner
 fun main(){
     val scanner = Scanner(System.`in`)
 
-    println("APLIKASI PMB UMN")
-    print("MAsukkan nama")
+    println("--- APLIKASI PMB UMN ---")
+    print("Masukkan nama")
     val name = scanner.nextLine()
 
     print("Masukkan NIM (Wajib 5 Karakter):")
@@ -17,10 +17,20 @@ fun main(){
     if (nim.length != 5){
         println("ERROR: Pendaftaran dibatalkan. NIM harus 5 karakter!")
     }else {
-        print("Masukkan jurusan:")
-        val major = scanner.nextLine()
+        print("Pilih jalur (1. Reguler 2.Umum): ")
+        val type = scanner.nextInt()
+        scanner.nextLine()
 
-        val s1 = Student(name = name, nim = nim, major = major)
-        println("Status: Pendaftaran selesai")
+        if (type == 1) {
+            print("Masukkan jurusan: ")
+            val major = scanner.nextLine()
+            val s1 = Student(name = name, nim = nim, major = major)
+            println("Terdaftar di: ${s1.major} dengan GPA awal ${s1.gpa}")
+        }else if (type == 2){
+            val s2 = Student(name, nim)
+            println("Terdaftar di ${s2.major} dengan GPA awal ${s2.gpa}")
+        }else{
+            println("Pilihan salah")
+        }
     }
 }
