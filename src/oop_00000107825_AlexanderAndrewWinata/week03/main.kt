5578f3190aa1689dd442a800b68f64428ea240bf
itwearmeout <crysero2@gmail.com>
2026-02-16 22:59:07 +0700

    week03: cleanup code

--- src/oop_00000107825_AlexanderAndrewWinata/week03/main.kt
+++ src/oop_00000107825_AlexanderAndrewWinata/week03/main.kt
@@ -2,9 +2,7 @@ package oop_00000107825_AlexanderAndrewWinata.week03
 
 fun main() {
     val e = Employee("Budi")
-
-    println("--- Test Error ---")
-    e.salary = -5000
+    
     e.salary = 5000000
     println("Gaji: ${e.salary}")
 
