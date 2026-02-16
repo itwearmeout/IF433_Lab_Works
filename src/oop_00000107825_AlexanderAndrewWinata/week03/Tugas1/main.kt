8d5a2deac0420f4499ce0fcca9da8e55b3a04288
itwearmeout <crysero2@gmail.com>
2026-02-16 23:18:42 +0700

    week03: (task 1) implement Weapon testing in main

--- src/oop_00000107825_AlexanderAndrewWinata/week03/Tugas1/main.kt
+++ src/oop_00000107825_AlexanderAndrewWinata/week03/Tugas1/main.kt
@@ -1,2 +1,9 @@
 package oop_00000107825_AlexanderAndrewWinata.week03.Tugas1
 
+fun main(){
+    val weap = Weapon("Sword")
+
+    weap.damage = -50
+
+    weap.damage = 9999
+}
--- src/oop_00000107825_AlexanderAndrewWinata/week03/Tugas1/weapon.kt
+++ src/oop_00000107825_AlexanderAndrewWinata/week03/Tugas1/weapon.kt
@@ -1,15 +1,16 @@
 package oop_00000107825_AlexanderAndrewWinata.week03.Tugas1
 
-class Weapon() {
-    val name: String = "Sword"
+class Weapon(var name: String) {
     var damage: Int = 0
         set(value) {
             if (value<0) {
                 println("Tidak boleh negatif!")
             }else if (value>1000){
                 field = 1000
+                println("Damage $name = $damage")
             }else{
                 field = value
+                println("Damage $name = $damage")
             }
         }
     init {
@@ -21,5 +22,6 @@ class Weapon() {
             var rarity :String = "legendary"
         }
     }
+
 
 }
