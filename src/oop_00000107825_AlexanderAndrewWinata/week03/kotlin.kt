e477b66a3616866154807a3e7968e8865fbedeaa
itwearmeout <crysero2@gmail.com>
2026-02-16 22:51:35 +0700

    week03: fix setter using backing field

--- src/oop_00000107825_AlexanderAndrewWinata/week03/kotlin.kt
+++ src/oop_00000107825_AlexanderAndrewWinata/week03/kotlin.kt
@@ -3,7 +3,10 @@ package oop_00000107825_AlexanderAndrewWinata.week03
 class Emploee(val name:String){
     var salary: Int = 0
         set(value) {
-            println("Mencoba set gaji ke $value")
-            this.salary = value
+            if (value<0) {
+                println("Mencoba set gaji ke $value")
+            }else {
+                field = value
+            }
         }
 }
