4d6f7328c80a4556507fc6417e6e7dfbf73b6fe7
itwearmeout <crysero2@gmail.com>
2026-02-16 22:49:57 +0700

    week03: simulate stack overflow error in setter

--- src/oop_00000107825_AlexanderAndrewWinata/week03/kotlin.kt
+++ src/oop_00000107825_AlexanderAndrewWinata/week03/kotlin.kt
@@ -2,4 +2,8 @@ package oop_00000107825_AlexanderAndrewWinata.week03
 
 class Emploee(val name:String){
     var salary: Int = 0
+        set(value) {
+            println("Mencoba set gaji ke $value")
+            this.salary = value
+        }
 }
---
+++ src/oop_00000107825_AlexanderAndrewWinata/week03/main.kt
@@ -0,0 +1,8 @@
+package oop_00000107825_AlexanderAndrewWinata.week03
+
+fun main() {
+    val e = Employee("Budi")
+
+    printlnn("--- Test Error ---")
+    e.salary = 5000
+}
