16dba7197dc59a1fb5692b3f0cfa8ac3d9bdf254
itwearmeout <crysero2@gmail.com>
2026-03-02 15:35:30 +0700

    week05: (task 1) test MathHelper overloading in main

--- src/oop_00000107825_AlexanderAndrewWinata/week05/Tugas1/main.kt
+++ src/oop_00000107825_AlexanderAndrewWinata/week05/Tugas1/main.kt
@@ -1,4 +1,8 @@
 package oop_00000107825_AlexanderAndrewWinata.week05.Tugas1
 
 fun main() {
+    val hitung = MathHelper()
+    println("Hitung lebar persegi ${hitung.hitungLuas(10)}")
+    println("Hitung lebar p panjang ${hitung.hitungLuas(10,12)}")
+    println("Hitung lebar lingkaran ${hitung.hitungLuas(200000.9999)}")
 }
