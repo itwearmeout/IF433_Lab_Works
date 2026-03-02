20dd8c824ae5f989d85376703a81d831f44da2ca
itwearmeout <crysero2@gmail.com>
2026-03-02 14:59:29 +0700

    week05: create abstract class Pegawai

---
+++ src/oop_00000107825_AlexanderAndrewWinata/week04/GuidedLab/Vehicle.kt
@@ -0,0 +1,15 @@
+package oop_00000107825_AlexanderAndrewWinata.week04.GuidedLab
+
+open class Vehicle (
+    val brand: String
+) {
+    var speed: Int = 0
+    open fun accelerate(){
+        speed += 10
+        println("$brand melaju, Kecepatan: $speed km/jam")
+    }
+
+    open fun honk(){
+        println("Beep beep!")
+    }
+}
---
+++ src/oop_00000107825_AlexanderAndrewWinata/week05/Pegawai.kt
@@ -0,0 +1,7 @@
+package oop_00000107825_AlexanderAndrewWinata.week05
+
+abstract class Pegawai (
+    val name: String
+) {
+    abstract  fun berkerja()
+}
