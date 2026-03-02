16b23d1d98a450636214403822451fe280457c60
itwearmeout <crysero2@gmail.com>
2026-03-02 15:29:19 +0700

    week05: (task 1) create MathHelper class with overloading

---
+++ src/oop_00000107825_AlexanderAndrewWinata/week05/Tugas1/MathHelper.kt
@@ -0,0 +1,15 @@
+package oop_00000107825_AlexanderAndrewWinata.week05.Tugas1
+
+class MathHelper {
+    fun hitungLuas(sisi: Int):Int {
+        return (sisi*sisi)
+    }
+
+    fun hitungLuas(panjang: Int, lebar: Int):Int {
+        return (panjang*lebar)
+    }
+
+    fun hitungLuas(jariJari: Double): Double {
+        return (3.14*jariJari*jariJari)
+    }
+}
---
+++ src/oop_00000107825_AlexanderAndrewWinata/week05/Tugas1/main.kt
@@ -0,0 +1,4 @@
+package oop_00000107825_AlexanderAndrewWinata.week05.Tugas1
+
+fun main() {
+}
