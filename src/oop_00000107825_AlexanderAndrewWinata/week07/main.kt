754118ab3fcdfa859dbc4463cbbcc2399714d4ed
Andrew Jack <crysero2@gmail.com>
2026-03-17 16:54:14 +0700

    week07: resolve sealed class exhaustive check

--- src/oop_00000107825_AlexanderAndrewWinata/week07/main.kt
+++ src/oop_00000107825_AlexanderAndrewWinata/week07/main.kt
@@ -31,6 +31,7 @@ fun main() {
     val response: ApiResponse = ApiResponse.Success("Data berhasil ditarik!")
 
     val uiMessage = when(response) {
+        is ApiResponse.Loading -> "Tampilkan Spinner"
         is ApiResponse.Success -> "Tampilkan: ${response.data}"
         is ApiResponse.Error -> "Munculkan alert: ${response.message}"
     }
