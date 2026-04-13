5e1847f075c70e9cdb9b214f769274672330e143
itwearmeout <crysero2@gmail.com>
2026-04-13 15:16:21 +0700

    week08: implement requireNotNull as safer alternative

--- src/oop_00000107825_AlexanderAndrewWinata/week08/Main.kt
+++ src/oop_00000107825_AlexanderAndrewWinata/week08/Main.kt
@@ -1,5 +1,7 @@
 package oop_00000107825_AlexanderAndrewWinata.week08
 
+import com.sun.jdi.connect.IllegalConnectorArgumentsException
+
 fun main(){
     println("=== TEST SAFE CALLS & ELVIS ====")
     val emptyOrder = Order(null, null)
@@ -36,4 +38,21 @@ fun main(){
     val someObject: Any = 100
     val safeString = someObject as? String ?: "Unknown String"
     println("Hasil cast + fallback :$safeString") //berhasil HOORAY
+
+    println("=== TEST THE RED BUTTON !! ===")
+    val toxicData: String? = null
+    try{
+        val length = toxicData!!.length
+    } catch (e: NullPointerException) {
+        println("CRASH NPE ! JANGAN GUNAKAN !! SECARA SEMBARANG")
+    }
+
+    val apiResponse: Map<String, String?> = mapOf("status" to "200", "token" to null)
+    try{
+        val token = requireNotNull(apiResponse["token"]) {
+            "CRITICAL EXCEPTION: Token otentilasi tidak ditemukan dari server!"
+        }
+    } catch (e: IllegalConnectorArgumentsException) {
+        println(e.message)
+    }
 }
