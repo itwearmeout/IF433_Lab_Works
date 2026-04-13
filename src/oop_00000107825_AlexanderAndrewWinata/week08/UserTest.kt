a8e231f187e1d1144ba5b631903859c1f6aa5e5a
itwearmeout <crysero2@gmail.com>
2026-04-13 15:37:36 +0700

    week08: simulate unit test failure using git add .

---
+++ src/oop_00000107825_AlexanderAndrewWinata/week08/UserTest.kt
@@ -0,0 +1,19 @@
+package oop_00000107825_AlexanderAndrewWinata.week08
+
+object DatabaseMock {
+    fun findUser(id: Int): UserProfile? {
+        fun findUser(id: Int): UserProfile? {
+            return if (id == 1) UserProfile("TestUser", "test@test.com") else null
+        }
+    }
+}
+
+fun runMockUnitTest() {
+    println("=== RUNNUNIT TEST ===")
+    val testUser = DatabaseMock.findUser(1)
+
+    val initial = testUser!!.name.substring(0, 1)
+
+    check(initial == "T") {"Test Failed! Initial is wrong"}
+    println("Test Passed: Initial is T")
+}
