0e4951e1b7839cb9beecd45ea9cb48878a9cd330
itwearmeout <crysero2@gmail.com>
2026-04-20 14:42:21 +0700

    week09: (task) populate mock trade history list

--- src/oop_00000107825_AlexanderAndrewWinata/TradingModels.kt
+++ src/oop_00000107825_AlexanderAndrewWinata/TradingModels.kt
@@ -1,3 +1,3 @@
-package oop_00000107825_AlexanderAndrewWinata
+package oop_00000107825_AlexanderAndrewWinata.week09
 
 data class TradeLog (val pair: String, val position: String, val leverage: Int, val roe: Double, val status: String)
---
+++ src/oop_00000107825_AlexanderAndrewWinata/week09/TradingDashboard.kt
@@ -0,0 +1,12 @@
+package oop_00000107825_AlexanderAndrewWinata.week09
+
+fun main() {
+    val tradeHistory = listOf(
+        TradeLog("BTCUSDT", "LONG", 20, 15.5, "CLOSED"),
+        TradeLog("ETHUSDT", "SHORT", 10, -5.2, "CLOSED"),
+        TradeLog("BTCUSDT", "SHORT", 50, 120.0, "CLOSED"),
+        TradeLog("SOLUSDT", "LONG", 10, -10.0, "CLOSED"),
+        TradeLog("ETHUSDT", "LONG", 20, 5.0, "OPEN"),
+        TradeLog("BNBUSDT", "SHORT", 5, -2.5, "CLOSED")
+    )
+}
