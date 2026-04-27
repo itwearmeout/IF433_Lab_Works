70534d6ffcde69bf0788095c2ced56c41bb05d73
itwearmeout <crysero2@gmail.com>
2026-04-27 13:04:11 +0700

    week10: instantiate Box with Int and String types

---
+++ src/oop_00000107825_AlexanderAndrewWinata/week10/main.kt
@@ -0,0 +1,9 @@
+package oop_00000107825_AlexanderAndrewWinata.week10
+
+fun main() {
+    println("=== TEST GENERIC CLASS ===")
+    val intBox = Box(100)
+    val stringBox = Box("Generics in Kotlin")
+    println("Isi intBox: ${intBox.value}")
+    println("Isi stringBox: ${stringBox.value}")
+}
