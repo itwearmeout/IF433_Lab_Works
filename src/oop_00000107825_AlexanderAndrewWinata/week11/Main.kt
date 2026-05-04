01d71c42167c6a88a8d61895f7d144c600718575
itwearmeout <crysero2@gmail.com>
2026-05-04 14:42:00 +0700

    week11: test extension functions in main

---
+++ src/oop_00000107825_AlexanderAndrewWinata/week11/Main.kt
@@ -0,0 +1,9 @@
+package oop_00000107825_AlexanderAndrewWinata.week11
+
+fun main() {
+    println("=== TEST EXTENSION FUNCTIONS ===")
+    println("Alex".addGreeting())
+    println("Hi".repeatTimes(3))
+    val text: String? = null
+    println("Apakah null/empty? ${text.isNullOrEmptyCustom()}")
+}
