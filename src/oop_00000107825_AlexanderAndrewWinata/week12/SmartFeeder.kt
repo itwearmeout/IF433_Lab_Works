da356deaa2e5f022ac52c6b95fc06cf29737bdd2
andre <crysero2@gmail.com>
2026-05-17 14:37:55 +0700

    week12: (task) execute feeding using runCatching

--- src/oop_00000107825_AlexanderAndrewWinata/week12/SmartFeeder.kt
+++ src/oop_00000107825_AlexanderAndrewWinata/week12/SmartFeeder.kt
@@ -25,4 +25,7 @@ fun main() {
     } finally {
         println("Siklus pengecekan dispenser pagi selesai.")
     }
+    runCatching { 
+        dispenseKibble(requestedGram = 30, availableGram = 1000, isJammed = false) 
+    }
 }
