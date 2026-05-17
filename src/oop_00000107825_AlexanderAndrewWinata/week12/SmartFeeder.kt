230e1544dcec6428fd52aff34e4ff9a20eee1f5f
andre <crysero2@gmail.com>
2026-05-17 14:44:22 +0700

    week12: (task) handle feeding failure using onFailure

--- src/oop_00000107825_AlexanderAndrewWinata/week12/SmartFeeder.kt
+++ src/oop_00000107825_AlexanderAndrewWinata/week12/SmartFeeder.kt
@@ -33,5 +33,5 @@ fun main() {
     }.onFailure { error ->
         println("Peringatan ke Pemilik: ${error.message}")
         println("(Opsional: Berikan chicken jerky secara manual)")
-    }
+    } //done
 }
